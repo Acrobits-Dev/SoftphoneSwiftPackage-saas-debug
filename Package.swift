76fd898901296134856941f704c70f267e1ffc36
Autobuild <autobuild@minichim4.inner.acrobits.cz>
2026-06-19 13:24:05 +0200

    Package updated to 25.3.25

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
         .binaryTarget(
             name: "Softphone",
             url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-25.3.25.zip",
-            checksum: "a65602a32cc068c564082fd3e511c39a4482991b7f0e033d25f7bd12f51a6df6"),
+            checksum: "0a970d0cb75bf5b41a23c9097172ad288129ebdb3ce49f8e389c0c902a5efd64"),
         .binaryTarget(
             name: "Softphone_Swift",
             url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-25.3.25.zip",
-            checksum: "d03d1b4e46f729820920e4f7dbdf6359d3c0bd64cb302d3ea96c4013f8938885"),
+            checksum: "c796e42d65f3a1869e16eb06efa0eb63f150087e31d5d1273821a44b163a891e"),
     ]
 )
