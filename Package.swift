2ee57b46c76fb9706f82e536094bc3a16907187c
Autobuild <autobuild@m1mini.local>
2026-09-16 12:14:20 +0200

    Package updated to 25.3.37

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-25.3.36.zip",
-            checksum: "a059a8dd1cb9f1a45a3853148bff585b2868350ff60626be74d1ca771af69211"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone.xcframework-25.3.37.zip",
+            checksum: "699a576fc5236f266d8354a073b53c0d524174d1094176ac6b5ea8d9f906db46"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-25.3.36.zip",
-            checksum: "62c5fe040bbd1a70c2fb2b49c9cea28a769fd2fcb12e6a90f4fc8957deb62298"),
+            url: "https://dist.acrobits.net/saas-swift-package/debug/Softphone_Swift.xcframework-25.3.37.zip",
+            checksum: "7195a19a4dea2d86e854ab8334ee79f732f895a6bc04c1eff9d7ba70b8dc7e5e"),
     ]
 )
